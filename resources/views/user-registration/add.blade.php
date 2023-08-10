bb8d622038b05c092e860521736ed17175aff5dc
Kaviyarasan_Shanmugam <kaviyarasan@processdrive.com>
2023-08-10 10:38:56 +0530

    issue fixed

--- resources/views/user-registration/add.blade.php
+++ resources/views/user-registration/add.blade.php
@@ -39,23 +39,23 @@
                     </div>
                 </div>
                 <div class="row form-group mt-3">
-                    <label for="emp_id" class="col-sm-4 font-weight-bold">Employee ID:</label>
+                    <label for="emp_id" class="col-sm-4 font-weight-bold">Employee ID:<span class="required">*</span></label>
                      <div class="col-sm-8 ">
-                         <input type="text" id="emp_id" name="emp_id" placeholder="Enter Employee ID" value="{{old('emp_id')}}"  class="form-control">
+                         <input type="text" id="emp_id" name="emp_id" placeholder="Enter Employee ID" value="{{old('emp_id')}}" required="required" class="form-control">
 
                     </div>
                 </div>
                 <div class="row form-group mt-3">
-                    <label for="phone" class="col-sm-4 font-weight-bold">Phone:</label>
+                    <label for="phone" class="col-sm-4 font-weight-bold">Phone:<span class="required">*</span></label>
                      <div class="col-sm-8">
-                         <input type="text" id="phone" name="phone" placeholder="Enter Phone" value="{{old('phone')}}"  class="form-control">
+                         <input type="text" id="phone" name="phone" placeholder="Enter Phone" value="{{old('phone')}}" required="required" class="form-control">
 
                     </div>
                 </div>
                 <div class="row form-group mt-3">
-                    <label for="email" class="col-sm-4 font-weight-bold">Email:</label>
+                    <label for="email" class="col-sm-4 font-weight-bold">Email:<span class="required">*</span></label>
                      <div class="col-sm-8 ">
-                         <input type="email" id="email" name="email" placeholder="Enter Email" value="{{old('email')}}" class="form-control">
+                         <input type="email" id="email" name="email" placeholder="Enter Email" value="{{old('email')}}" required="required" class="form-control">
 
                     </div>
                 </div>
@@ -68,9 +68,9 @@
                 </div>
 
                 <div class="row form-group mt-3">
-                    <label for="location" class="col-sm-4 font-weight-bold">Location:</label>
+                    <label for="location" class="col-sm-4 font-weight-bold">Location:<span class="required">*</span></label>
                      <div class="col-sm-8 ">
-                         <select id="location" name="location" class="form-control">
+                         <select id="location" name="location" class="form-control" required="required">
                             <option value="" selected> -- Select location to -- </option>
                             @foreach ($location as $id => $name)
                                 <option value="{{$id}}"  > {{$name}} </option>
@@ -80,9 +80,9 @@
                 </div>
 
                 <div class="row form-group mt-3">
-                    <label for="department" class="col-sm-4 font-weight-bold">Department:</label>
+                    <label for="department" class="col-sm-4 font-weight-bold">Department:<span class="required">*</span></label>
                      <div class="col-sm-8 ">
-                         <select id="department" name="department"  class="form-control">
+                         <select id="department" name="department"  class="form-control" required="required">
                             <option value="" selected> -- Select Department -- </option>
                             @foreach ($departments as $department)
                            <option value="{{$department->id}}"  > {{$department->department}} </option>
@@ -92,9 +92,9 @@
                 </div>
 
                 <div class="row form-group mt-3">
-                    <label for="affiliated_to" class="col-sm-4 font-weight-bold">Affiliated to:</label>
+                    <label for="affiliated_to" class="col-sm-4 font-weight-bold">Affiliated to:<span class="required">*</span></label>
                      <div class="col-sm-8 ">
-                         <select id="affiliated_to" name="affiliated_to" class="form-control">
+                         <select id="affiliated_to" name="affiliated_to" class="form-control" required="required">
                             <option value="" selected> -- Select Affiliated to -- </option>
                             @foreach ($affiliated_to as $id => $name)
                                 <option value="{{$id}}"  > {{$name}} </option>
